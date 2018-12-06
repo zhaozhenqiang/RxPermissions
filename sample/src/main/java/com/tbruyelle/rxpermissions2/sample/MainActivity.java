8a03cfbed56e25f6f38cf608c7e49cb24a60f82d
liujing <12609480@qq.com>
2018-12-06 17:22:24 +0800

    bug

--- sample/src/main/java/com/tbruyelle/rxpermissions2/sample/MainActivity.java
+++ sample/src/main/java/com/tbruyelle/rxpermissions2/sample/MainActivity.java
@@ -6,9 +6,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.SurfaceView;
+import android.view.View;
 import android.widget.Toast;
+
 import com.gaodun.commonlib.permission.OnPermissionCallback;
 import com.gaodun.commonlib.permission.PermissionManager;
+
 import java.io.IOException;
 
 import io.reactivex.disposables.Disposable;
@@ -24,37 +27,45 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_main);
+        surfaceView = findViewById(R.id.surfaceView);
 
-
-        PermissionManager.instance().with(MainActivity.this).request(new OnPermissionCallback() {
+        findViewById(R.id.enableCamera).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onRequestAllow(String permissionName) {
-                if (Manifest.permission.CAMERA.equals(permissionName)) {
-                    releaseCamera();
-                    camera = Camera.open(0);
-                    try {
-                        camera.setPreviewDisplay(surfaceView.getHolder());
-                        camera.startPreview();
-                    } catch (IOException e) {
-                        Log.e(TAG, "Error while trying to display the camera preview", e);
+            public void onClick(View view) {
+
+                PermissionManager.instance().with(MainActivity.this).request(new OnPermissionCallback() {
+                    @Override
+                    public void onRequestAllow(String permissionName) {
+                        if (Manifest.permission.CAMERA.equals(permissionName)) {
+                            releaseCamera();
+                            camera = Camera.open(0);
+                            try {
+                                camera.setPreviewDisplay(surfaceView.getHolder());
+                                camera.startPreview();
+                            } catch (IOException e) {
+                                Log.e(TAG, "Error while trying to display the camera preview", e);
+                            }
+                        }
                     }
-                }
-            }
 
-            @Override
-            public void onRequestRefuse(String permissionName) {
-                Toast.makeText(MainActivity.this,
-                        "Denied permission without ask never again",
-                        Toast.LENGTH_SHORT).show();
-            }
+                    @Override
+                    public void onRequestRefuse(String permissionName) {
+                        Toast.makeText(MainActivity.this,
+                                "Denied permission without ask never again",
+                                Toast.LENGTH_SHORT).show();
+                    }
 
-            @Override
-            public void onRequestNoAsk(String permissionName) {
-                Toast.makeText(MainActivity.this,
-                        "Permission denied, can't enable the camera",
-                        Toast.LENGTH_SHORT).show();
+                    @Override
+                    public void onRequestNoAsk(String permissionName) {
+                        Toast.makeText(MainActivity.this,
+                                "Permission denied, can't enable the camera",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }, Manifest.permission.CAMERA);
             }
-        }, Manifest.permission.CAMERA);
+        });
+
     }
 
     @Override
